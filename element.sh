0ac26ff55e3fdfe6f2d9ec2097bf662248f01843
venomous-007 <152641321+venomous-007@users.noreply.github.com>
2026-09-19 07:28:08 -0400

    chore: get_the_element_info

--- element.sh
+++ element.sh
@@ -3,4 +3,5 @@ PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
-fi
+else
+  ELEMENT_INFO=$($PSQL "select * from elements where atomic_number::text='$1' or symbol='$1' or name='$1'")
