77420b1b64b400eee53fb427000a936009758928
venomous-007 <152641321+venomous-007@users.noreply.github.com>
2026-09-19 07:47:32 -0400

    fix: variable_wrong_assignment

--- element.sh
+++ element.sh
@@ -1,4 +1,4 @@
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 if [[ -z $1 ]]
 then
@@ -10,3 +10,11 @@ else
   then
    echo "I could not find that element in the database."
   else
+    ATOMIC_MASS=$($PSQL "select atomic_mass from properties where atomic_number='$ATOMIC_NUMBER'")
+    MELTING_POINT=$($PSQL "select melting_point_celsius from properties where atomic_number='$ATOMIC_NUMBER'")
+    BOILING_POINT=$($PSQL "select boiling_point_celsius from properties where atomic_number='$ATOMIC_NUMBER'")
+    TYPE_ID=$($PSQL "select type_id from properties where atomic_number='$ATOMIC_NUMBER'")
+    TYPE=$($PSQL "select type from types where type_id='$TYPE_ID'")
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  fi
+fi
