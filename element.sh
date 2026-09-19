8de8f1254d29afcca685165aa0cd0a1c080dbafb
venomous-007 <152641321+venomous-007@users.noreply.github.com>
2026-09-19 07:37:45 -0400

    fix: read_the_element_info_variables

--- element.sh
+++ element.sh
@@ -5,7 +5,7 @@ then
   echo "Please provide an element as an argument."
 else
   ELEMENT_INFO=$($PSQL "select * from elements where atomic_number::text='$1' or symbol='$1' or name='$1'")
-
+  IFS="|" read ATOMIC_NUMBER SYMBOL NAME <<< "$ELEMENT_INFO"
   if [[ -z $ELEMENT_INFO ]]
   then
    echo "I could not find that element in the database."
